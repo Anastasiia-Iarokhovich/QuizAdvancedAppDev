3049417f9b80501f6a90703b0e5401e8234a866a
Anastasiia Iarokhovich <anastasiia.iarokhovich@stud.hshl.de>
2023-10-19 19:33:04 +0200

    first commit

---
+++ src/app/Models/Quiz.ts
@@ -0,0 +1,9 @@
+export class Quiz
+{
+    constructor(
+        public id:number,
+        public question:string,
+        public options:string[],
+        public correctOptionIndex:number
+    ) {}
+}
